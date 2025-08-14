5b4d805c1703d63a6b33def30be21979bd59c485
mamun2519 <juborajislam46@gmail.com>
2025-08-14 23:46:03 +0600

    Feat: added to module services

---
+++ src/services/module.services.ts
@@ -0,0 +1,35 @@
+import ApiError from "../error/apiError";
+import { IModule } from "../interface/module.interface";
+import { Module } from "../models/module.model";
+
+const getAllModule = async (): Promise<IModule[]> => {
+  const modules: IModule[] = await Module.find({});
+  return modules;
+};
+const getAllModuleByCourseId = async (courseId: string): Promise<IModule[]> => {
+  const modules: IModule[] = await Module.find({ courseId });
+  return modules;
+};
+
+const getModuleByIdFromDb = async (id: string): Promise<IModule | null> => {
+  const modules: IModule | null = await Module.findById(id);
+  if (!modules) {
+    throw new ApiError(404, "Module not found");
+  }
+  return modules;
+};
+
+const deleteModuleFromDb = async (id: string): Promise<IModule | null> => {
+  const modules: IModule | null = await Module.findByIdAndDelete(id);
+  if (!Module) {
+    throw new ApiError(404, "Module not found");
+  }
+  return modules;
+};
+
+export const ModuleServices = {
+  deleteModuleFromDb,
+  getAllModule,
+  getAllModuleByCourseId,
+  getModuleByIdFromDb,
+};
