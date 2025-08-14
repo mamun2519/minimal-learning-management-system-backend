1ec061c85b8d1d5c605db1efbdf271952017ae9c
mamun2519 <juborajislam46@gmail.com>
2025-08-14 22:17:54 +0600

    Feat: added to course interface and services

---
+++ src/interface/course.interface.ts
@@ -0,0 +1,14 @@
+import { Model } from "mongoose";
+
+export type ICourse = {
+  _id: string;
+  title: string;
+  description: string;
+  price: number;
+  file: {
+    url: string;
+    key: string;
+  };
+};
+
+export type CourseModel = Model<ICourse>;
