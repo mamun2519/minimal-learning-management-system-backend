a97c131a35456e50b131786799f86286391d4cf2
mamun2519 <juborajislam46@gmail.com>
2025-08-15 16:05:06 +0600

    Feat: added to lecture added services

--- src/services/module.services.ts
+++ src/services/module.services.ts
@@ -1,7 +1,96 @@
+import { Request } from "express";
 import ApiError from "../error/apiError";
+import { ILecture } from "../interface/lecture.interface";
 import { IModule } from "../interface/module.interface";
 import { Module } from "../models/module.model";
+import { Course } from "../models/course.model";
+import { StatusCodes } from "http-status-codes";
+import cloudinary from "../config/cloudinary";
 
+// const insertModuleAndLectureIntoDB = async (
+//   req: Request,
+//   module: IModule,
+//   lecture: ILecture[]
+// ) => {
+//   try {
+//     const course = await Course.findById(module.courseId);
+//     if (!course) {
+//       throw new ApiError(
+//         StatusCodes.NOT_FOUND,
+//         "Requested Course Does Not Found"
+//       );
+//     }
+
+//     const fileUploadResult = req.files?.map(async (file) => {
+//       const uploadFile = await cloudinary.uploader.upload(file.path, {
+//         folder: "lms_uploads",
+//         resource_type: "auto",
+//       });
+//       return uploadFile;
+//     });
+//     console.log(fileUploadResult);
+//     const uploadModule = await Module.create(module);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+export const insertModuleAndLectureIntoDB = async (
+  req: Request,
+  module: IModule,
+  lectures: ILecture[]
+) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    const course = await Course.findById(module.courseId).session(session);
+    if (!course) {
+      throw new ApiError(
+        StatusCodes.NOT_FOUND,
+        "Requested Course Does Not Found"
+      );
+    }
+
+    // Upload files to Cloudinary
+    const uploadedFiles = await Promise.all(
+      (req.files as Express.Multer.File[]).map(async (file) => {
+        const uploaded = await cloudinary.uploader.upload(file.path, {
+          folder: "lms_uploads",
+          resource_type: "auto",
+        });
+        return {
+          url: uploaded.secure_url,
+          key: uploaded.public_id,
+        };
+      })
+    );
+
+    // Create Module
+    const newModule = await Module.create([module], { session });
+    const moduleId = newModule[0]._id;
+
+    // Attach files to lectures based on your matching logic
+    // Here, assuming all uploaded files are pdfNotes for now
+    const lectureDocs = lectures.map((lec) => ({
+      ...lec,
+      moduleId,
+      courseId: module.courseId,
+      pdfNotes: uploadedFiles,
+    }));
+
+    await Lecture.insertMany(lectureDocs, { session });
+
+    await session.commitTransaction();
+    session.endSession();
+
+    return { module: newModule[0], lectures: lectureDocs };
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    throw error;
+  }
+};
 const getAllModuleFromDB = async (): Promise<IModule[]> => {
   const modules: IModule[] = await Module.find({});
   return modules;
