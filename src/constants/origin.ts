e37be0112086e06c6eb4a9bd24cad5ede7800d72
mamun2519 <juborajislam46@gmail.com>
2025-08-19 16:36:54 +0600

    Feat: added to cors

--- src/constants/origin.ts
+++ src/constants/origin.ts
@@ -1,13 +1,6 @@
 export const corsOptions = [
-  "http://localhost:5173",
+  "https://minimal-learning-management-system-nine.vercel.app/",
   "http://localhost:3000",
-  "http://localhost:4173",
-  "https://shops.joybanglamakemore.com",
-  // "http://localhost:5174",
-  // "http://209.126.4.170"
-  // "https://ytshops.co",
-  // "https://ytshops.com",
-  // "http://127.0.0.1:5173"
 ];
 
 export const corsMethods = ["GET", "POST", "PUT", "DELETE", "PATCH"];
