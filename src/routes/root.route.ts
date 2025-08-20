1771067d97247e3c0943571c1da615392f2585b6
mamun2519 <juborajislam46@gmail.com>
2025-08-20 16:36:54 +0600

    remove unnesaru file

--- src/routes/root.route.ts
+++ src/routes/root.route.ts
@@ -1,10 +1,6 @@
 import express from "express";
 import { AuthRoute } from "./auth.route";
 import notFoundAPI from "../middlewares/notFound";
-import { CourseRoute } from "./course.route";
-import { ModuleRoute } from "./module.route";
-import { LectureRoute } from "./lecture.route";
-import { EnrolledCourseRoute } from "./enrolledCourse.route";
 
 const router = express.Router();
 // Application Module Route ---------
@@ -12,22 +8,6 @@ const moduleRoute = [
   {
     path: "/auth",
     route: AuthRoute,
-  },
-  {
-    path: "/course",
-    route: CourseRoute,
-  },
-  {
-    path: "/module",
-    route: ModuleRoute,
-  },
-  {
-    path: "/lecture",
-    route: LectureRoute,
-  },
-  {
-    path: "/enrolled-course",
-    route: EnrolledCourseRoute,
   },
 ];
 
