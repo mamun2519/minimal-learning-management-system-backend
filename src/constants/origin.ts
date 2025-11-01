d93bb3900d1d0dea0d5b16eb58137424f597756b
mamun2519 <juborajislam46@gmail.com>
2025-11-01 16:07:52 +0600

    feat: change to origin

--- src/constants/origin.ts
+++ src/constants/origin.ts
@@ -1,6 +1,7 @@
 export const corsOptions = [
   "https://minimal-learning-management-system-nine.vercel.app",
   "http://localhost:3000",
+  "http://45.136.19.151",
 ];
 
 export const corsMethods = ["GET", "POST", "PUT", "DELETE", "PATCH"];
